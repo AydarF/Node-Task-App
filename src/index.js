d8a0b1df717f2af461938c0df7cd86dca74a5623
AydarF <aydar_fayzullin@yahoo.com>
2020-04-18 23:50:21 -0400

    Create reading endpoints

--- src/index.js
+++ src/index.js
@@ -9,6 +9,31 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
 app.post("/users", (req, res) => {
   const user = new User(req.body);
 
@@ -18,7 +43,7 @@ app.post("/users", (req, res) => {
       res.status(201).send(user);
     })
     .catch((error) => {
-      res.status(400).send(error);
+      res.status(500).send();
     });
 });
 
@@ -36,5 +61,5 @@ app.post("/tasks", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is up and running pn port ", +port);
+  console.log("Server is up and running on port ", +port);
 });
