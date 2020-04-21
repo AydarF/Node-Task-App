f7f45a3d3a0f12ee30287dad2ff672137b875d47
AydarF <aydar_fayzullin@yahoo.com>
2020-04-20 21:43:58 -0400

    Create update tasks endpoint

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ app.patch("/users/:id", async (req, res) => {
   );
 
   if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid operation!" });
+    return res.status(400).send({ error: "Uh-oh! Invalid operation!" });
   }
 
   try {
@@ -98,6 +98,32 @@ app.get("/tasks/:id", async (req, res) => {
     res.status(200).send(task);
   } catch (error) {
     res.status(500).send();
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Uh-oh! Invalid updates!" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
