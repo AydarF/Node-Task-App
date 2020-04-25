3edb1285e322a928a24050c7baec650035d3eb8d
AydarF <aydar_fayzullin@yahoo.com>
2020-04-24 23:24:54 -0400

    Create user authentication and logout endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 const User = require("../models/user");
+const auth = require("../middleware/auth.js");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -28,13 +29,21 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
   } catch (error) {
     res.status(500).send();
   }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
