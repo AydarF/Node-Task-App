f39354e2665e8166309ce35ae5b43da7052ef681
AydarF <aydar_fayzullin@yahoo.com>
2020-05-29 00:00:37 -0400

    Send a welcomeemail

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,16 +4,16 @@ const sendGridAPIKey = process.env.SEND_GRID_API_KEY;
 
 sgMail.setApiKey(sendGridAPIKey);
 
-sgMail
-  .send({
-    to: "faidarik@mail.ru",
-    from: "ayrfay@gmail.com",
-    subject: "Setting up SendGrid",
-    text: "I hope this works",
-  })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((error) => {
-    console.log(error.response.body);
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "andrew@mead.io",
+    subject: "Thanks for joining in!",
+    text: `Welcome to the app, ${name}.
+    Let me know how you get along with the app.`,
   });
+};
+
+module.export = {
+  sendWelcomeEmail,
+};
